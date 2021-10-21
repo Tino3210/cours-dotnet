ee7169f9a3a76bb79f4eb8eeca4d3583ef2c2634
Izzo Valentino <valentino.izzo@he-arc.ch>
2021-10-21 21:26:05 +0200

    exercice 3

---
+++ Serie1/Exercice3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace Exercice3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 0;
+            
+            string path = @".\Mesures.txt";
+            try
+            {
+                using (StreamReader sr = new StreamReader(path))
+                {
+                    while (sr.Peek() >= 0)
+                    {
+                        counter++;
+                        String mesure = sr.ReadLine();
+                        if (counter % 10 == 0)
+                        {
+                            Console.WriteLine($"{@mesure}");
+                        }
+                        else
+                        {
+                            Console.Write($"{@mesure}" + ", ");
+                        }                       
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The process failed: {0}", e.ToString());
+            }
+        }
+    }
+}

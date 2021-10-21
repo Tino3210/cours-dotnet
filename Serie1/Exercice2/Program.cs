298412d238525642262b50fcbd6462968d386883
Izzo Valentino <valentino.izzo@he-arc.ch>
2021-10-21 20:59:43 +0200

    exercice 2

---
+++ Serie1/Exercice2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Exercice2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nombre réel positif : ");
+            double reel = 0;
+            reel = Double.Parse(Console.ReadLine());
+            DateTime dt1 = new DateTime();
+            int nbApro = ApproSqrt(reel, reel);
+            DateTime dt2 = new DateTime();
+            Console.WriteLine("Temps écoulé : " + dt2.Subtract(dt1));
+            Console.WriteLine("Nombre d'apporimation : " + nbApro);
+        }
+
+        public static int ApproSqrt(double A, double apro)
+        {
+            int nbApro = 1;
+            double newApro = (apro + A / apro) / 2;
+            Console.WriteLine("Approximation de la racine carrée de " + A + " est " + Math.Round(newApro,3));
+            if (newApro - Math.Sqrt(A) > (A*Math.Pow(10, -9)))
+            {
+                nbApro++;
+                ApproSqrt(A, newApro);
+            }
+            return nbApro;
+        }
+    }
+}
